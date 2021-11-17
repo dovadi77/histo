e0f7c19fd76fcdba51a64b4e8b47503be59e2f3a
Fernando Gunawan <fernando@dihardjasoftware.com>
2021-11-17 14:18:21 +0700

    fix: add missing folder

--- app/Models/Leaderboard.php
+++ app/Models/Leaderboard.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Leaderboard extends Model
 {
     use HasFactory;
 }
